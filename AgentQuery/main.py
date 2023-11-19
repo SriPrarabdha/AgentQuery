a4009ef9ce1cec4180191bf41d3154b9323be3c3
Prarabdha <prabodhkumarsrivastav70@gmail.com>
2023-11-19 12:42:16 +0530

    completed Postgress db added license, env

--- AgentQuery/main.py
+++ AgentQuery/main.py
@@ -1,5 +1,18 @@
+import os
+from AgentQuery.modules.db import PostgressDb
+import dotenv
+
+dotenv.load_dotenv()
+
+assert os.environ.get("DATABASE_URL"), "DATABASE_URL not found"
+assert os.environ.get("OPENAI_API_KEY"), "OPENAI_API_KEY not found"
+
+DB_URL = os.environ.get("DATABASE_URL")
+OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY")
+
+
 def main():
-    print("hello ji")
+    with 
     pass
 
 
--- AgentQuery/modules/db.py
+++ AgentQuery/modules/db.py
@@ -1,5 +1,7 @@
 import psycopg2
 from psycopg2.sql import SQL, Identifier
+from datetime import datetime 
+import json
 
 class PostgressDb():
     def __init__(self):
@@ -68,8 +70,108 @@ class PostgressDb():
         self.curr.execute(sql)
         return self.curr.fetchall()
     
+    def datetime_handler(self, obj):
+        """
+        Handle datetime objects when serializing to JSON.
+        """
+        if isinstance(obj, datetime):
+            return obj.isoformat()
+        return str(obj) 
+
     def get_table_definition(self, table_name):
-        get_def_stmt = ""
+        """
+        Generate the 'create' definition for a table
+        """
+
+        get_def_stmt = """
+        SELECT pg_class.relname as tablename,
+            pg_attribute.attnum,
+            pg_attribute.attname,
+            format_type(atttypid, atttypmod)
+        FROM pg_class
+        JOIN pg_namespace ON pg_namespace.oid = pg_class.relnamespace
+        JOIN pg_attribute ON pg_attribute.attrelid = pg_class.oid
+        WHERE pg_attribute.attnum > 0
+            AND pg_class.relname = %s
+            AND pg_namespace.nspname = 'public'  -- Assuming you're interested in public schema
+        """
+        self.cur.execute(get_def_stmt, (table_name,))
+        rows = self.cur.fetchall()
+        create_table_stmt = "CREATE TABLE {} (\n".format(table_name)
+        for row in rows:
+            create_table_stmt += "{} {},\n".format(row[2], row[3])
+        create_table_stmt = create_table_stmt.rstrip(",\n") + "\n);"
+        return create_table_stmt
+
+    def get_all_table_names(self):
+        get_all_tables_stmt = (
+            "SELECT tablename FROM pg_tables WHERE schemaname = 'public';"
+        )
+        self.cur.execute(get_all_tables_stmt)
+        return [row[0] for row in self.cur.fetchall()]
+
+    def get_table_definitions_for_prompt(self):
+        table_names = self.get_all_table_names()
+        definitions = []
+        for table_name in table_names:
+            definitions.append(self.get_table_definition(table_name))
+        return "\n\n".join(definitions)
+
+    def get_table_definition_map_for_embeddings(self):
+        table_names = self.get_all_table_names()
+        definitions = {}
+        for table_name in table_names:
+            definitions[table_name] = self.get_table_definition(table_name)
+        return definitions
+
+    def get_related_tables(self, table_list, n=2):
+
+        related_tables_dict = {}
+
+        for table in table_list:
+            # Query to fetch tables that have foreign keys referencing the given table
+            self.cur.execute(
+                """
+                SELECT 
+                    a.relname AS table_name
+                FROM 
+                    pg_constraint con 
+                    JOIN pg_class a ON a.oid = con.conrelid 
+                WHERE 
+                    confrelid = (SELECT oid FROM pg_class WHERE relname = %s)
+                LIMIT %s;
+                """,
+                (table, n),
+            )
+
+            related_tables = [row[0] for row in self.cur.fetchall()]
+
+            self.cur.execute(
+                """
+                SELECT 
+                    a.relname AS referenced_table_name
+                FROM 
+                    pg_constraint con 
+                    JOIN pg_class a ON a.oid = con.confrelid 
+                WHERE 
+                    conrelid = (SELECT oid FROM pg_class WHERE relname = %s)
+                LIMIT %s;
+                """,
+                (table, n),
+            )
+
+            related_tables += [row[0] for row in self.cur.fetchall()]
+
+            related_tables_dict[table] = related_tables
+            
+        related_tables_list = []
+        for table, related_tables in related_tables_dict.items():
+            related_tables_list += related_tables
+
+        related_tables_list = list(set(related_tables_list))
+
+        return related_tables_list
+
 
 
     
